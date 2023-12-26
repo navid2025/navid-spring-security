8aace7967e08e4d134fb9262559ebeea947c75ed
n.zare <n.zare@behsacorp.com>
2023-12-26 17:27:38 +0330

    first commit

---
+++ src/main/java/com/navid/navidspringsecurity/NavidSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.navid.navidspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavidSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavidSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navid/navidspringsecurity/NavidSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navid.navidspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavidSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
