b2dbfced671b52510d37f447cf2bd2a5c1dc4c18
navid2025 <69727440+navid2025@users.noreply.github.com>
2023-12-30 17:06:06 +0330

    Update NavidSpringSecurityApplication.java

--- src/main/java/com/navid/navidspringsecurity/NavidSpringSecurityApplication.java
+++ src/main/java/com/navid/navidspringsecurity/NavidSpringSecurityApplication.java
@@ -8,6 +8,7 @@ public class NavidSpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NavidSpringSecurityApplication.class, args);
+        
     }
 
 }
